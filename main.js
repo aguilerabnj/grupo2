2ffbb695b4a417e6c17ecc9bbb5e3b76a0d518dc
aguilerabnj <aguilerabnj@gmail.com>
2025-09-09 16:23:03 -0300

    cambios en main.js

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-//navbar
+
+class Persona {
+	constructor(nombre, dni, edad) {
+		this.nombre = nombre
+		this.dni = dni
+		this.edad = edad
+		this.productos = []
+	}
+	/* adperson(){
+		console.log(`la persona es ${this.nombre} su dni es ${this.dni} y su edad es ${this.edad}`)
+	} */
+
+	agregarproducto(producto) {
+		this.productos.push(producto)
+	}
+	eliminarproducto(producto) {
+		let indice = this.productos.findIndex(p => p === producto)
+		this.productos.splice(indice, 1)
+	}
+	reset() {
+		this.productos = []
+	}
+	datos() {
+		return `Nombre: ${this.nombre} DNI: ${this.dni} edad: ${this.edad}`
+	}
+	subtotal() {
+		let acum = 0
+		this.productos.forEach(x => {
+			acum = acum + x.precio
+
+
+		});
+			return acum * (1.21)
+	}
+
+
+}
