e74e4fb1fea112ea47e0d6fc1a5489253cd68b87
aguilerabnj <aguilerabnj@gmail.com>
2025-09-15 16:50:23 -0300

    yfghjfjhg

---
+++ main.js
@@ -0,0 +1,10 @@
+class Producto{
+	constructor (id,nombre, precio, descripcion, stock, marca){
+		this.id = id
+		this.nombre = nombre
+		this.precio = precio
+		this.marca = marca
+		this.descripcion = descripcion
+		this.stock = stock
+	}
+}
