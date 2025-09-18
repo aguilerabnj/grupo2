fb1b894bb86e770e8ebc0ca6bf4ea2b24861498f
aguilerabnj <aguilerabnj@gmail.com>
2025-09-18 15:25:40 -0300

    inicio de creacion de la tarjeta (js)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-class Producto{
-	constructor (id,nombre, precio, descripcion, stock, marca){
+class Producto {
+	constructor(id, nombre, precio, descripcion, stock, marca) {
 		this.id = id
 		this.nombre = nombre
 		this.precio = precio
@@ -7,7 +7,15 @@ class Producto{
 		this.descripcion = descripcion
 		this.stock = stock
 	}
-datos() {
+
+	datos() {
 		return `id ${this.id} nombre: ${this.nombre} precio: ${this.precio} descripcion: ${this.descripcion} stock: ${this.stock}`
 	}
 }
+
+
+let contenedor = document.getElementById("contenedor")
+let tarjeta = document.getElementById("tarjeta")
+let titulo = document.createElement("h1")
+titulo.textContent = "paco rabanne"
+contenedor.appendChild(tarjeta)
